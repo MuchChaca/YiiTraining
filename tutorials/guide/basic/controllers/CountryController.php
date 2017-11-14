4db0477df9d1f83c2ad159bc841446b043ea6d6f
archey <chk-make@gmail.com>
2017-11-14 06:17:57 +0000

    :arrow up: Added Controller for Country
     Changes to be committed:
    	new file:   controllers/CountryController.php

---
+++ tutorials/guide/basic/controllers/CountryController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+use yii\data\Pagination;
+use app\models\Country;
+
+class CountryController extends Controller{
+
+	public function actionIndex(){
+		//
+		$query = Country::find();	// get all the data from the Country table (?)
+
+		$pagination = new Pagination([
+			'defaultPageSize' => 5,
+			'totalCount' => $query->count(),  // $query->count() counts the number of rows returned by the query $query
+		]);
+
+		$countries = $query->orderBy('name')
+			->offset($pagination->offset)
+			->limit($pagination->limit)
+			->all();
+
+		return $this->render('index', [
+			'pagination' => $pagination,
+			'countries' => $countries
+		]);
+	}
+}
