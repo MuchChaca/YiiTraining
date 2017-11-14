7e51d878f1e438d897d78e543dc1816496e98516
archey <chk-make@gmail.com>
2017-11-14 10:37:52 +0000

    Generated (kinda) the ActiveRecord

--- tutorials/guide/basic/models/Country.php
+++ tutorials/guide/basic/models/Country.php
@@ -5,19 +5,53 @@
 
  // No need to write anything inside Country class
  // Yii guess the table name associated
- class Country extends ActiveRecord{
- }
-
-
+ /** 
+* This is the model class for table "country". 
+* 
+* @property string $code 
+* @property string $name 
+* @property integer $population 
+*/ 
+class Country extends \yii\db\ActiveRecord 
+{ 
+   /** 
+    * @inheritdoc 
+    */ 
+   public static function tableName() 
+   { 
+       return 'country'; 
+   } 
 // get all rows from the country table and order them by "name"
 // $countries = Country::find()->orderBy('name')->all();
-
+   /**
+    * @inheritdoc
+    */
+   public function rules()
+   {
+       return [
+           [['code', 'name'], 'required'],
+           [['population'], 'integer'],
+           [['code'], 'string', 'max' => 2],
+           [['name'], 'string', 'max' => 52],
+       ];
+   }
 // get the row whose primary key is "US"
 // $country = Country::findOne('US');
-
-// displays "United States"
-// echo $country->name;
-
-// modifies the country name to be "U.S.A." and save it to database
-// $country->name = 'U.S.A.';
+   /**
+    * @inheritdoc
+    */
+   public function attributeLabels()
+   {
+       return [
+           'code' => 'Code',
+           'name' => 'Name',
+           'population' => 'Population',
+       ];
+   }
+}
+// displays "United States" 
+// echo $country->name; 
+ 
+// modifies the country name to be "U.S.A." and save it to database 
+// $country->name = 'U.S.A.'; 
 // $country->save();
