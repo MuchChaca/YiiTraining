21654707925eff57e0f528f1abb7fc47faaf01dd
archey <chk-make@gmail.com>
2017-11-14 11:34:19 +0000

    CRUD generated
     Changes to be committed:
    	modified:   controllers/CountryController.php
    	new file:   models/CountrySearch.php
    	new file:   views/country/_form.php
    	new file:   views/country/_search.php
    	new file:   views/country/create.php
    	modified:   views/country/index.php
    	new file:   views/country/update.php
    	new file:   views/country/view.php

--- tutorials/guide/basic/controllers/CountryController.php
+++ tutorials/guide/basic/controllers/CountryController.php
@@ -2,29 +2,123 @@
 
 namespace app\controllers;
 
-use yii\web\Controller;
-use yii\data\Pagination;
+use Yii;
 use app\models\Country;
+use app\models\CountrySearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
 
-class CountryController extends Controller{
+/**
+ * CountryController implements the CRUD actions for Country model.
+ */
+class CountryController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
 
-	public function actionIndex(){
-		//
-		$query = Country::find();	// get all the data from the Country table (?)
+    /**
+     * Lists all Country models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new CountrySearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
-		$pagination = new Pagination([
-			'defaultPageSize' => 5,
-			'totalCount' => $query->count(),  // $query->count() counts the number of rows returned by the query $query
-		]);
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
 
-		$countries = $query->orderBy('name')
-			->offset($pagination->offset)
-			->limit($pagination->limit)
-			->all();
+    /**
+     * Displays a single Country model.
+     * @param string $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
 
-		return $this->render('index', [
-			'pagination' => $pagination,
-			'countries' => $countries
-		]);
-	}
+    /**
+     * Creates a new Country model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Country();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->code]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing Country model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param string $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->code]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Country model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param string $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Country model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param string $id
+     * @return Country the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Country::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
 }
--- tutorials/guide/basic/views/country/index.php
+++ tutorials/guide/basic/views/country/index.php
@@ -1,15 +1,34 @@
 <?php
+
 use yii\helpers\Html;
-use yii\widgets\LinkPager;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\CountrySearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Countries';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
+<div class="country-index">
 
-<h1>Countries</h1>
-<ul>
-<?php foreach($countries as $country): ?>
-	<li>
-		<?= Html::encode("{$country->name} ({$country->code})") ?>: <?= $country->population ?>
-	</li>
-<?php endforeach; ?>
-</ul>
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-<?= LinkPager::widget(['pagination' => $pagination]) ?>
+    <p>
+        <?= Html::a('Create Country', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'code',
+            'name',
+            'population',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
