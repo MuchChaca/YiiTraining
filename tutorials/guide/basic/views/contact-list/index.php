79145b6bffe0dc866b664aa000edc16112c49da0
archey <chk-make@gmail.com>
2017-11-15 05:54:05 +0000

    View for ContactList done (~)
     On branch not-generated
     Changes to be committed:
    	new file:   views/contact-list/index.php

---
+++ tutorials/guide/basic/views/contact-list/index.php
@@ -0,0 +1,17 @@
+<?php
+use yii\helpers\Html;
+use yii\widgets\LinkPager;
+?>
+
+<h1>Contacts</h1>
+<ul>
+	<?php foreach($contacts as $contact): ?>
+		<li>
+			<?= Html::encode("{$contact->nom} {$contact->prenom} ") ?>
+			<strong>Email: </strong><?= Html::encode("{$contact->email}") ?><br>
+			<strong>Portable: </strong><?= Html::encode("{$contact->portable}") ?><br>
+			<strong>Fixe: </strong><?= Html::encode("{$contact->fixe}") ?>
+		</li><br>
+	<?php endforeach; ?>
+</ul>
+<?= LinkPager::widget(['pagination' => $pagination]) ?>
