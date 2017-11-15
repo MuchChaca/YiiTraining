65c7f778f0e347d30437174b6f7061b53ccbb048
archey <chk-make@gmail.com>
2017-11-15 10:33:38 +0000

    Save
     On branch not-generated
     Changes to be committed:
    	modified:   views/contact-list/index.php

--- tutorials/guide/basic/views/contact-list/index.php
+++ tutorials/guide/basic/views/contact-list/index.php
@@ -1,9 +1,12 @@
 <?php
 use yii\helpers\Html;
 use yii\widgets\LinkPager;
+use yii\web\Link;
+
 ?>
 
 <h1>Contacts</h1>
+// TODO: Ajouter lien vers la creation e contact
 <ul>
 	<?php foreach($contacts as $contact): ?>
 		<li>
